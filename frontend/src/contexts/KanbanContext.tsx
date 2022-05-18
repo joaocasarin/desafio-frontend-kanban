54e097efff786134f88b8a5099117a838bba5f36
Joao Casarin <joaovitorcasarin@hotmail.com>
2022-05-18 02:13:14 -0300

    moved all kanban related methods to its own context

--- frontend/src/contexts/KanbanContext.tsx
+++ frontend/src/contexts/KanbanContext.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import { createContext, useState } from 'react';
-import { KanbanContextProps, CreatedTask, KanbanProviderProps } from '../interfaces';
+import { KanbanContextProps, CreatedTask, KanbanProviderProps, Task } from '../interfaces';
 import { api } from '../utils';
 
 export const KanbanContext = createContext({} as KanbanContextProps);
 
 export const KanbanProvider = ({ children }: KanbanProviderProps) => {
     const [tasks, setTasks] = useState<CreatedTask[]>([]);
+
     const getTasks = async (): Promise<CreatedTask[]> => {
         const token = localStorage.getItem('token');
         const response = await api.get('/cards', {
@@ -15,14 +16,92 @@ export const KanbanProvider = ({ children }: KanbanProviderProps) => {
             }
         });
 
-        const { data } = response;
+        if (response.status === 401) {
+            throw new Error('Invalid credentials');
+        }
+
+        const { data } = response as { data: CreatedTask[] };
+
+        setTasks(data);
+        return data;
+    };
+
+    const createTask = async (task: Task): Promise<CreatedTask> => {
+        const token = localStorage.getItem('token');
+        const response = await api.post('/cards', task, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 401) {
+            throw new Error('Invalid credentials');
+        }
+
+        if (response.status === 400) {
+            throw new Error('Invalid task');
+        }
+
+        const { data } = response as { data: CreatedTask };
+
+        setTasks([...tasks, data]);
+
+        return data;
+    };
+
+    const updateTask = async (task: CreatedTask): Promise<CreatedTask> => {
+        const token = localStorage.getItem('token');
+        const response = await api.put(`/cards/${task.id}`, task, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 400) {
+            throw new Error('Invalid task');
+        }
+
+        if (response.status === 401) {
+            throw new Error('Invalid credentials');
+        }
+
+        if (response.status === 404) {
+            throw new Error('Task not found');
+        }
+
+        const { data } = response as { data: CreatedTask };
+
+        setTasks(tasks.map((oldTask) => (oldTask.id === data.id ? data : oldTask)));
+
+        return data;
+    };
+
+    const deleteTask = async (task: CreatedTask): Promise<CreatedTask[]> => {
+        const token = localStorage.getItem('token');
+        const response = await api.delete(`/cards/${task.id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 401) {
+            throw new Error('Invalid credentials');
+        }
+
+        if (response.status === 404) {
+            throw new Error('Task not found');
+        }
+
+        const { data } = response as { data: CreatedTask[] };
 
         setTasks(data);
 
-        console.log(data);
-
-        return (data as CreatedTask[]) || ([] as CreatedTask[]);
+        return data;
     };
 
-    return <KanbanContext.Provider value={{ tasks, getTasks }}>{children}</KanbanContext.Provider>;
+    return (
+        <KanbanContext.Provider value={{ tasks, getTasks, createTask, updateTask, deleteTask }}>
+            {children}
+        </KanbanContext.Provider>
+    );
 };
--- frontend/src/contexts/UserContext.tsx
+++ frontend/src/contexts/UserContext.tsx
@@ -1,21 +1,14 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import { createContext, useState } from 'react';
-import {
-    LoginResponseData,
-    UserContextProps,
-    UserProviderProps,
-    CreatedTask,
-    Task
-} from '../interfaces';
+import { LoginResponseData, UserContextProps, UserProviderProps } from '../interfaces';
 import { api } from '../utils';
 
 export const UserContext = createContext({} as UserContextProps);
 
 export const UserProvider = ({ children }: UserProviderProps) => {
     const token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibGV0c2NvZGUiLCJpYXQiOjE2NTI3MzcwNTcsImV4cCI6MTY1MjgyMzQ1N30.lPpNJyxGtuHii6pQjl7M4_UtL3TrfrTjDq94v-WMryw';
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibGV0c2NvZGUiLCJpYXQiOjE2NTI4NDA0NjYsImV4cCI6MTY1Mjg0NDA2Nn0.LJBipEuDzZN8sQJ7i68aXMPpK9vJqN6iV7tr9bK4z9I';
     const [isLoading, setIsLoading] = useState(false);
-    const [tasks, setTasks] = useState<CreatedTask[]>([]);
 
     const login = async (username: string, password: string) => {
         setIsLoading(true);
@@ -51,111 +44,13 @@ export const UserProvider = ({ children }: UserProviderProps) => {
         return response.status === 200;
     };
 
-    const getTasks = async () => {
-        setIsLoading(true);
-
-        const response = await api.get('/cards', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-
-        const { data, status } = response;
-
-        if (status !== 200) {
-            return undefined;
-        }
-
-        setIsLoading(false);
-        setTasks(data);
-
-        return data as CreatedTask[];
-    };
-
-    const createTask = async (task: Task) => {
-        setIsLoading(true);
-
-        const response = await api.post('/cards', task, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-
-        const { data, status } = response;
-
-        if (status !== 201) {
-            return undefined;
-        }
-
-        getTasks();
-        setIsLoading(false);
-
-        return data as CreatedTask;
-    };
-
-    const deleteTask = async (id: string) => {
-        setIsLoading(true);
-
-        const response = await api.delete(`/cards/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-
-        const { data, status } = response;
-
-        if (status !== 200) {
-            return undefined;
-        }
-
-        getTasks();
-        setIsLoading(false);
-
-        return data as CreatedTask[];
-    };
-
-    const updateTask = async ({ id, conteudo, lista, titulo }: CreatedTask) => {
-        setIsLoading(true);
-
-        const response = await api.put(
-            `/cards/${id}`,
-            {
-                conteudo,
-                lista,
-                titulo,
-                id
-            } as CreatedTask,
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }
-        );
-
-        const { data, status } = response;
-
-        if (status !== 200) {
-            return undefined;
-        }
-
-        getTasks();
-        setIsLoading(false);
-
-        return data as CreatedTask;
-    };
-
     return (
         <UserContext.Provider
             value={{
                 login,
                 isLoggedIn,
                 logout,
-                isLoading,
-                createTask,
-                deleteTask,
-                getTasks,
-                updateTask,
-                tasks
+                isLoading
             }}
         >
             {children}
