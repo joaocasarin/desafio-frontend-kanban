a15b5a613b7f1227e601ec8ba1d06e464c48b561
Joao Casarin <joaovitorcasarin@hotmail.com>
2022-05-18 02:29:27 -0300

    added features to the task cards

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,15 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styled from 'styled-components';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import EditIcon from '@mui/icons-material/Edit';
 import VisibilityIcon from '@mui/icons-material/Visibility';
-import { Divider, IconButton } from '@mui/material';
+import { Divider, IconButton, Typography } from '@mui/material';
 import randomcolor from 'randomcolor';
-import { CreatedTask } from '../../interfaces/Task';
-import { UserContext } from '../../contexts/UserContext';
+import Modal from '../Modal';
+import { CreatedTask } from '../../interfaces';
 import { COLUMNS } from '../../utils';
+import { KanbanContext } from '../../contexts/KanbanContext';
 
 const Card = styled.div`
     width: 80%;
@@ -35,9 +36,22 @@ const Buttons = styled.div`
 `;
 
 const CardComponent = ({ task }: { task: CreatedTask }) => {
-    const { id, conteudo, lista, titulo } = task;
-    const { updateTask } = useContext(UserContext);
+    const { titulo, lista } = task;
+    const { updateTask, deleteTask } = useContext(KanbanContext);
     const ColumnsCount = COLUMNS.length;
+
+    const [message, setMessage] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalDisplayType, setModalDisplayType] = useState<'edit' | 'view' | 'error' | 'success'>(
+        'success'
+    );
+
+    const onOpenModal = (displayMode: 'edit' | 'view' | 'error' | 'success') => {
+        setModalDisplayType(displayMode);
+        setIsModalOpen(true);
+    };
+
+    const onCloseModal = () => setIsModalOpen(false);
 
     const moveToLeft = async () => {
         const posIndex = COLUMNS.indexOf(lista);
@@ -46,12 +60,20 @@ const CardComponent = ({ task }: { task: CreatedTask }) => {
                 ? COLUMNS[posIndex - 1]
                 : COLUMNS[ColumnsCount - 1];
 
-        await updateTask({
-            id,
-            conteudo,
-            lista: newPosition,
-            titulo
-        });
+        try {
+            return await updateTask({
+                ...task,
+                lista: newPosition
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                setMessage(error.message);
+            } else {
+                setMessage('An unknown error occurred');
+            }
+
+            return onOpenModal('error');
+        }
     };
 
     const moveToRight = async () => {
@@ -59,37 +81,75 @@ const CardComponent = ({ task }: { task: CreatedTask }) => {
         const newPosition =
             posIndex < ColumnsCount - 1 && posIndex > -1 ? COLUMNS[posIndex + 1] : COLUMNS[0];
 
-        await updateTask({
-            id,
-            conteudo,
-            lista: newPosition,
-            titulo
-        });
+        try {
+            return await updateTask({
+                ...task,
+                lista: newPosition
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                setMessage(error.message);
+            } else {
+                setMessage('An unknown error occurred');
+            }
+
+            return onOpenModal('error');
+        }
+    };
+
+    const onDeleteTask = async () => {
+        try {
+            return await deleteTask(task);
+        } catch (error) {
+            if (error instanceof Error) {
+                setMessage(error.message);
+            } else {
+                setMessage('An unknown error occurred');
+            }
+
+            return onOpenModal('error');
+        }
     };
 
     return (
-        <Card style={{ backgroundColor: randomcolor() }}>
+        <Card style={{ backgroundColor: randomcolor({ luminosity: 'light' }) }}>
             <Buttons>
-                {titulo}
-                <IconButton aria-label='visualize' size='small' onClick={() => console.log(task)}>
+                <Typography variant='subtitle1' marginLeft={2}>
+                    {titulo}
+                </Typography>
+
+                <IconButton aria-label='visualize' size='small' onClick={() => onOpenModal('view')}>
                     <VisibilityIcon fontSize='inherit' />
                 </IconButton>
             </Buttons>
+
             <Divider variant='middle' />
+
             <Buttons>
                 <IconButton aria-label='moveToLeft' size='small' onClick={moveToLeft}>
                     <ArrowCircleLeftIcon fontSize='inherit' />
                 </IconButton>
-                <IconButton aria-label='delete' size='small' onClick={() => console.log(task)}>
+
+                <IconButton aria-label='delete' size='small' onClick={onDeleteTask}>
                     <RemoveCircleIcon fontSize='inherit' />
                 </IconButton>
-                <IconButton aria-label='edit' size='small' onClick={() => console.log(task)}>
+
+                <IconButton aria-label='edit' size='small' onClick={() => onOpenModal('edit')}>
                     <EditIcon fontSize='inherit' />
                 </IconButton>
+
                 <IconButton aria-label='moveToRight' size='small' onClick={moveToRight}>
                     <ArrowCircleRightIcon fontSize='inherit' />
                 </IconButton>
             </Buttons>
+
+            <Modal
+                displayType={modalDisplayType}
+                isOpen={isModalOpen}
+                onClose={onCloseModal}
+                task={task}
+                message={message}
+            />
         </Card>
     );
 };
