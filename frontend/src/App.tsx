c6d858f11a74ee63f4229ffb1dde9c70216fc982
Joao Casarin <joaovitorcasarin@hotmail.com>
2022-05-18 05:06:45 -0300

    redirect all other pages to home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Others from './pages/Others';
 
 const App = () => (
     <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='*' element={<Others />} />
     </Routes>
 );
 
---
+++ frontend/src/pages/Others/index.tsx
@@ -0,0 +1,5 @@
+import { Navigate } from 'react-router-dom';
+
+const Others = () => <Navigate to='/' replace />;
+
+export default Others;
